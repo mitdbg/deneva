168d5991831e75d694c8a14bd9cce76a521674c5
rharding6373 <rhardin@mit.edu>
2016-05-24 00:59:22 -0400

    Allow other locking CC algos to be used in lower isolation levels

--- system/main.cpp
+++ system/main.cpp
@@ -78,10 +78,6 @@ int main(int argc, char* argv[])
 	srand(seed);
 	printf("Random seed: %ld\n",seed);
 
-#if ISOLATION_LEVEL == READ_COMMITTED
-  assert(CC_ALG == NO_WAIT);
-#endif
-
 	int64_t starttime;
 	int64_t endtime;
   starttime = get_server_clock();
